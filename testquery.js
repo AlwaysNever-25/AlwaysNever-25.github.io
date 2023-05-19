60210c037eae67f26ba5b660d7432847afc495db
AlwaysNever-25 <alwaysnever2000@gmail.com>
2023-05-19 18:42:37 +0700

    fixing homepage with js

--- testquery.js
+++ testquery.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+    $(".portal_sliderlink-2").on("click", function() { // binding click event
+        window.location = "index.html";
+      });
+    $(".portal_sliderlink-3").on("click", function() { // binding click event
+        window.location = "/whatisthis/notindex.html";
+      });
 
+    $(".portal_sliderlink-4").on("click", function() { // binding click event
+        window.location = "/bootcamp/index.html";
+      });
+    
+    $(".portal_sliderlink-5").on("click", function() { // binding click event
+        window.location = "about.html";
+      });
 });
