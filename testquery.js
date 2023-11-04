9dd572008af4f33c5d43722d0489abfabbf8922a
AlwaysNever-25 <83473354+AlwaysNever-25@users.noreply.github.com>
2023-11-04 20:49:06 +0700

    testing non-jquery js #3 pls work

--- testquery.js
+++ testquery.js
@@ -1,14 +1,15 @@
 document.querySelector(".portal_sliderlink-2").addEventListener("click", function() { // binding click event
-        window.location.assign("index.html");
+        location.assign("index.html");
       });
+
 document.querySelector(".portal_sliderlink-3").addEventListener("click", function() { // binding click event
-        window.location.assign("/whatisthis/notindex.html");
+        location.assign("/whatisthis/notindex.html");
       });
 
 document.querySelector(".portal_sliderlink-4").addEventListener("click", function() { // binding click event
-        window.location.assign("/fcreviews/fcreviews.html");
+        location.assign("/fcreviews/fcreviews.html");
       });
     
 document.querySelector(".portal_sliderlink-5").addEventListener("click", function() { // binding click event
-        window.location.assign("about.html");
+        location.assign("about.html");
       });
