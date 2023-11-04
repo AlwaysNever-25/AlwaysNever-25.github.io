ba4076dc5437c820f9cc49f9d47a82bc01e916de
AlwaysNever-25 <83473354+AlwaysNever-25@users.noreply.github.com>
2023-11-04 21:22:03 +0700

    testing non-jquery js #5

--- testquery.js
+++ testquery.js
@@ -1,15 +1,51 @@
-document.querySelector(".portal_sliderlink-2").addEventListener("click", function() { // binding click event
-        location.assign("index.html");
-      });
+// $(document).ready(function(){
+//     $(".portal_sliderlink-2").on("click", function() { // binding click event
+//         window.location = "index.html";
+//       });
+//     $(".portal_sliderlink-3").on("click", function() { // binding click event
+//         window.location = "/whatisthis/notindex.html";
+//       });
 
-document.querySelector(".portal_sliderlink-3").addEventListener("click", function() { // binding click event
-        location.assign("/whatisthis/notindex.html");
-      });
-
-document.querySelector(".portal_sliderlink-4").addEventListener("click", function() { // binding click event
-        location.assign("/fcreviews/fcreviews.html");
-      });
+//     $(".portal_sliderlink-4").on("click", function() { // binding click event
+//         window.location = "/fcreviews/fcreviews.html";
+//       });
     
-document.querySelector(".portal_sliderlink-5").addEventListener("click", function() { // binding click event
-        location.assign("about.html");
-      });
+//     $(".portal_sliderlink-5").on("click", function() { // binding click event
+//         window.location = "about.html";
+//       });
+// });
+
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => { 
+  var sliderlink_2 = document.createElement("div");
+  document.querySelector(".portal_sliderlink-2").appendChild(sliderlink_2);
+  // Add an event listener to the element
+  sliderlink_2.addEventListener("click", function()
+    { window.location.href = "index.html";
+  });
+
+  var sliderlink_3 = document.createElement("div");
+  document.querySelector(".portal_sliderlink-3").appendChild(sliderlink_3);
+  // Add an event listener to the element
+  sliderlink_3.addEventListener("click", function()
+    { window.location.href = "/whatisthis/notindex.html";
+  });
+
+  var sliderlink_4 = document.createElement("div");
+  document.querySelector(".portal_sliderlink-4").appendChild(sliderlink_4);
+  // Add an event listener to the element
+  sliderlink_4.addEventListener("click", function()
+    { window.location.href = "/fcreviews/fcreviews.html";
+  });
+
+  var sliderlink_5 = document.createElement("div");
+  document.querySelector(".portal_sliderlink-5").appendChild(sliderlink_5);
+  // Add an event listener to the element
+  sliderlink_5.addEventListener("click", function()
+    { window.location.href = "about.html";
+  });
+});
