65339f608fb92409c73cd20ddb41b34174cb8f28
AlwaysNever-25 <alwaysnever2000@gmail.com>
2024-09-09 02:44:45 +0700

    fix: UTC hour and minute not showing zero when it's one number

--- js/date.js
+++ js/date.js
@@ -1,10 +1,15 @@
 const desiredRepo = "AlwaysNever-25.github.io";
-  const dateTagClass = ".date"; // The HTML element you want to update
-  const monthList = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const dateTagClass = ".date"; // The HTML element you want to update
+const monthList = ["January","February","March","April","May","June","July", "August","September","October","November","December"];
 
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function()
-  {
+function addZero(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+}
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function()
+{
   if (this.readyState == 4 && this.status == 200)
   {
       let repos = JSON.parse(this.responseText);
@@ -16,13 +21,14 @@ const desiredRepo = "AlwaysNever-25.github.io";
           var day = lastUpdated.getUTCDate();
           var month = lastUpdated.getUTCMonth();
           var year = lastUpdated.getUTCFullYear();
-          var hour = lastUpdated.getUTCHours();
-          var minutes = lastUpdated.getUTCMinutes();
+          var hour = addZero(lastUpdated.getUTCHours());
+          var minutes = addZero(lastUpdated.getUTCMinutes());
           let monthText = monthList[month]
           $(dateTagClass).text(`Last updated: ${hour}:${minutes} UTC, ${monthText} ${day}, ${year}`);
       }
       });
   }
-  };
-  xhttp.open("GET", "https://api.github.com/users/alwaysnever-25/repos", true);
-  xhttp.send();
+};
+
+xhttp.open("GET", "https://api.github.com/users/alwaysnever-25/repos", true);
+xhttp.send();
